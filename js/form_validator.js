e1d9c321918e2cdb51fe84cb8a75b88101c7a5bd
mlmedia <millionlightsmedia@gmail.com>
2015-09-28 22:09:34 -0400

    - url regex to JS and matching fields markup

--- js/form_validator.js
+++ js/form_validator.js
@@ -136,6 +136,15 @@
 			}
 		});
 
+		/* check for valid URL */
+		function is_valid_url(string) {
+			var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+			if (regex.test(string) === true) {
+				return true;
+			}
+			return false;
+		}
+
 		/* check for alpha-numeric */
 		function is_alphanumeric(string) {
 			var regex = /^[A-Za-z0-9_.-]+$/;
